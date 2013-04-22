7c5b12e8fdb7cf6cacde2cf0e81a5676440c06cf
noughts <noughts@gmail.com>
2013-04-23 06:33:00 +0900

    表示パフォーマンスの改善のため、サムネイルを正方形にしました

--- iphone/Classes/JpDividualCapturedeviceFinderProxy.m
+++ iphone/Classes/JpDividualCapturedeviceFinderProxy.m
@@ -47,6 +47,7 @@ AVCaptureStillImageOutput* imageOutput;
     return resizedImage;
 }
 
+
 -(void)takePhoto:(id)args{
     NSLog( @"takePhoto" );
 	AVCaptureConnection *connection = [[imageOutput connections] lastObject];
@@ -63,7 +64,8 @@ AVCaptureStillImageOutput* imageOutput;
                                                  TiBlob* content_blob = [[[TiBlob alloc] initWithData:content_data mimetype:@"image/jpeg"] autorelease];
                                                  
                                                  // サムネイルのjpegデータを作成
-                                                 UIImage* thumbnail_img = [self resizeImage:content_img rect:self.view.bounds];
+//                                                 UIImage* thumbnail_img = [self resizeImage:content_img rect:CGRectMake(0, 0, 150, 150)];
+                                                 UIImage* thumbnail_img = [self imageByScalingAndCropping:content_img ForSize:CGSizeMake(150, 150)];
                                                  NSData *thumbnail_data = UIImageJPEGRepresentation( thumbnail_img, 0.8 );
                                                  TiBlob* thumbnail_blob = [[[TiBlob alloc] initWithData:thumbnail_data mimetype:@"image/jpeg"] autorelease];
                                                  
@@ -110,6 +112,64 @@ AVCaptureStillImageOutput* imageOutput;
     
     // セッション開始
     [captureSession startRunning];
+}
+
+
+
+- (UIImage*)imageByScalingAndCropping:(UIImage*)image ForSize:(CGSize)targetSize {
+    UIImage *sourceImage = image;
+    UIImage *newImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = targetSize.width;
+    CGFloat targetHeight = targetSize.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0,0.0);
+    
+    if (CGSizeEqualToSize(imageSize, targetSize) == NO){
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if (widthFactor > heightFactor)   {
+            scaleFactor = widthFactor; // scale to fit height
+        } else {
+            scaleFactor = heightFactor; // scale to fit width
+        }
+        
+        scaledWidth  = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        // center the image
+        if (widthFactor > heightFactor){
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+        }else{
+            if (widthFactor < heightFactor){
+                thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+            }
+        }
+    }
+    
+    UIGraphicsBeginImageContext(targetSize); // this will crop
+    
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width  = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    if(newImage == nil){
+        NSLog(@"could not scale image");
+    }
+    
+    //pop the context to get back to the default
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 
