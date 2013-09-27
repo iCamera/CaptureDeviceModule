885a6a70dd282d782ee987cee356084101aca55c
Koichi Yamamoto <noughts@gmail.com>
2013-09-28 02:18:52 +0900

    不要なコード削除

--- iphone/Classes/BeK0sukeTicameraView.h
+++ iphone/Classes/BeK0sukeTicameraView.h
@@ -32,22 +32,6 @@
 @property (nonatomic, strong) AVAssetWriterInputPixelBufferAdaptor *recordingAdaptor;
 @property (nonatomic, strong) NSTimer *intervalTimer;
 
--(id)isFrontCamera:(id)args;
--(id)isBackCamera:(id)args;
--(id)isTorch:(id)args;
 
--(void)startCamera:(id)args;
--(void)stopCamera:(id)args;
-
--(void)toggleCamera:(id)args;
--(void)toggleTorch:(id)args;
-
--(void)takePicture:(id)args;
--(void)startRecording:(id)args;
--(void)stopRecording:(id)args;
--(void)startInterval:(id)args;
--(void)stopInterval:(id)args;
-
-- (void)focusAndExposureAtPoint:(id)args;
 
 @end
